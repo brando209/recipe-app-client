a6ffb6fb735f681e01f577ce7326789afdb385c4
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-13 18:06:48 -0800

    Commit to test submodule

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Home from './pages/Home/Home';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Home />
     </div>
   );
 }
