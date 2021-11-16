ae8a54c4c516d5f8c0a612e11972833738e8d434
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-16 14:39:58 -0800

    Created Page component that wraps children in bootstraps Container component and applies styles.

---
+++ src/pages/Page/Page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './Page.css';
+
+export default function Page({ children, className, ...props }) {
+    return (
+        <Container className={`page ${className}`} {...props}>
+            { children }
+        </Container>
+    );
+}
