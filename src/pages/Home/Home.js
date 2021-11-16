ed76568be95c7787fab65071741139e4f0ee044c
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-16 14:51:17 -0800

    Added RecipeCard component to Home page component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Page from '../Page/Page';
+import RecipeCard from '../../components/app/RecipeCard/RecipeCard';
 import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
 
 export default function Home() {
     const { data, loading, error } = useRecipeContext();
 
     return (
-        <div>
+        <Page>
             Home page
             {loading && <p>Loading...</p>}
-            {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+            {error && <pre>{JSON.stringify(error.message, null, 2)}</pre>}
+            {!error && !loading && data.map(recipe => (
+                <RecipeCard key={recipe.id} {...recipe} />
+            ))}
+        </Page>
     )   
 }
