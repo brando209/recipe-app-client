80dba6aa1d55cf2e092fe4200e702c8c1b8fbca5
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-17 13:49:08 -0800

    Created `InputContainer` that wraps an input with a label and error message.

---
+++ src/components/input/InputContainer/InputContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ErrorMessage } from 'formik';
+import { Row, Col } from 'react-bootstrap';
+import './InputContainer.css';
+
+export default function InputContainer({ name, label, children }) {
+    return (
+        <Row className="input-container">
+            <Col xs={3}>
+                <label htmlFor={name}>{label}</label>
+            </Col>
+            { children.length ? 
+                children.map((child, index) => <Col key={index} className="mx-2">{child}</Col>) : 
+                <Col className="mx-2">{children}</Col>
+            }
+            <ErrorMessage name={name} component="div" className="form-error-message" />
+        </Row>
+    );
+}
