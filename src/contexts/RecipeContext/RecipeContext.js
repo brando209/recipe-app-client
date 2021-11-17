104ca5590155e8402341dc9051119d070fe56afc
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-17 13:52:54 -0800

    Added `createRecipe` function to `RecipeContext` component

--- src/contexts/RecipeContext/RecipeContext.js
+++ src/contexts/RecipeContext/RecipeContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from 'react';
 import useResource from '../../hooks/useResource';
+import recipeApi from '../../api/recipes';
 
 export const recipeContext = createContext({});
 
@@ -8,8 +9,18 @@ export const useRecipeContext = () => useContext(recipeContext);
 export default function RecipeContextProvider({ children }) {
     const { loading, error, value } = useResource('http://localhost:3005/api/recipes');
 
+    const createRecipe = async (recipeInfo, callback) => {
+        try {
+            const recipe = await recipeApi.createRecipe(recipeInfo);
+            callback(recipe.data, null);
+        } catch(err) {
+            console.error("Error:", err);
+            callback(null, err);
+        }
+    }
+
     return (
-        <recipeContext.Provider value={{ loading, error, data: value }}>
+        <recipeContext.Provider value={{ loading, error, data: value, createRecipe }}>
             {children}
         </recipeContext.Provider>
     )
