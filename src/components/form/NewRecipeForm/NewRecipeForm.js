3ef1b5582018bbc92e0110cee6f593166b284757
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-17 14:05:15 -0800

    Added `NewRecipeForm` to `NewRecipePage`

---
+++ src/components/form/NewRecipeForm/NewRecipeForm.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Inputs from '../../input';
+import { Button } from 'react-bootstrap';
+import './NewRecipeForm.css';
+
+const initialValues = {
+    title: "",
+    description: "",
+    serves: 1,
+    prep: {
+        time: "",
+        unit: "min"
+    },
+    cook: {
+        time: "",
+        unit: "min"
+    },
+    ingredients: [],
+    instructions: [],
+    comments: [],
+    categories: []
+}
+
+export default function NewRecipeForm({ onSubmit }) {
+    return (
+        <Formik
+            initialValues={initialValues}
+            onSubmit={(values, form) => {
+                form.setSubmitting(true);
+                onSubmit(values, () => {
+                    form.setSubmitting(false);
+                });
+            }}
+        >
+            {({ isSubmitting, values }) => (
+                <Form>
+                    <Inputs.InputContainer name="title" label="Title:">
+                        <Field name="title" placeholder="Recipe Title" type="input" />
+                    </Inputs.InputContainer>
+
+                    <Inputs.InputContainer name="description" label="Description:">
+                        <Field name="description" placeholder="Recipe Description" as="textarea" />
+                    </Inputs.InputContainer>
+
+                    <Inputs.InputContainer name="serves" label="Serves:">
+                        <Field name="serves" placeholder="Serves" type="number" />
+                    </Inputs.InputContainer>
+
+                    <Inputs.InputContainer name="prep" label="Prep:">
+                        <Field name="prep.time" type="number" />
+                        <Field name="prep.unit" as={Inputs.SelectInput} options={['min', 'hr']} />
+                    </Inputs.InputContainer>
+
+                    <Inputs.InputContainer name="cook" label="Cook:">
+                        <Field name="cook.time" type="number" />
+                        <Field name="cook.unit" as={Inputs.SelectInput} options={['min', 'hr']} />
+                    </Inputs.InputContainer>
+
+                    <Inputs.InputList
+                        name="ingredients" label="Ingredients:"
+                        listItems={values.ingredients}
+                        initialItemValue={{ name: "", amount: "", measurement: "", size: "" }}
+                        renderItem={(item, index, arrayHelpers) => <Inputs.IngredientInput key={`ingredient-${index}`} item={item} index={index} arrayHelpers={arrayHelpers} />}
+                    />
+
+                    <Inputs.InputList
+                        name="instructions" label="Instructions:"
+                        listItems={values.instructions}
+                        initialItemValue=""
+                        renderItem={(item, index, arrayHelpers) => <Inputs.InstructionInput key={`instruction-${index}`} item={item} index={index} arrayHelpers={arrayHelpers} />}
+                    />
+
+                    <Inputs.InputList
+                        name="comments" label="Comments:"
+                        listItems={values.comments}
+                        initialItemValue=""
+                        renderItem={(item, index, arrayHelpers) => <Inputs.CommentInput key={`comment-${index}`} item={item} index={index} arrayHelpers={arrayHelpers} />}
+                    />
+
+                    <Button type="submit" disabled={isSubmitting}>Submit</Button>
+                </Form>
+            )}
+        </Formik>
+    )
+}
--- src/pages/NewRecipePage/NewRecipePage.js
+++ src/pages/NewRecipePage/NewRecipePage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Page from '../Page/Page';
+import NewRecipeForm from '../../components/form/NewRecipeForm/NewRecipeForm';
+import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
+import { useNavigate } from 'react-router';
 
 export default function NewRecipePage(props) {
+    const { createRecipe } = useRecipeContext();
+    const navigate = useNavigate();
+
+    const handleCreateRecipe = (recipeInfo, callback) => {
+        createRecipe(recipeInfo, (recipe, err) => {
+            if(err) console.error(err);
+            callback();
+            navigate('/');
+        });
+    }
+
     return (
-        <div>New recipe page</div>
+        <Page>
+            New recipe page
+            <NewRecipeForm onSubmit={handleCreateRecipe}/>    
+        </Page>
+        
     );
 }
