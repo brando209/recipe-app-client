f500fac25b795d00d2d4454cf7007c3e0e0a6cae
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-29 15:30:40 -0800

    Add filter to recipe context

--- src/contexts/RecipeContext/RecipeContext.js
+++ src/contexts/RecipeContext/RecipeContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import useResource from '../../hooks/useResource';
 import recipeApi from '../../api/recipes';
 
@@ -8,7 +8,27 @@ export const useRecipeContext = () => useContext(recipeContext);
 
 export default function RecipeContextProvider({ children }) {
     const { loading, error, value } = useResource('http://localhost:3005/api/recipes');
+    const [filter, setFilter] = useState({ ingredients: [] });
 
+    const applyFilter = (filter) => {
+        const { ingredients } = filter;
+
+        let result = value;
+
+        //Filter by ingredients, result will include recipes that have all ingredients
+        if(ingredients) {
+            for(let ingredient of ingredients) {
+                result = result.filter(recipe => {
+                    const index = recipe.ingredients.findIndex(ing => ing.id === ingredient.id)
+                    return index > -1;
+                });
+            }
+        }
+
+        return result;
+    }
+
+    
     const createRecipe = async (recipeInfo, callback) => {
         try {
             const recipe = await recipeApi.createRecipe(recipeInfo);
@@ -19,8 +39,10 @@ export default function RecipeContextProvider({ children }) {
         }
     }
 
+    const filteredValue = applyFilter(filter);
+    
     return (
-        <recipeContext.Provider value={{ loading, error, data: value, createRecipe }}>
+        <recipeContext.Provider value={{ loading, error, data: filteredValue, createRecipe, updateFilter: setFilter, filter }}>
             {children}
         </recipeContext.Provider>
     )
