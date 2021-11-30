f506152c0496e2fc71b58b28db2d8c2126edacff
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-30 12:36:39 -0800

    Added 'search' field to filter state

--- src/contexts/RecipeContext/RecipeContext.js
+++ src/contexts/RecipeContext/RecipeContext.js
@@ -8,26 +8,41 @@ export const useRecipeContext = () => useContext(recipeContext);
 
 export default function RecipeContextProvider({ children }) {
     const { loading, error, value } = useResource('http://localhost:3005/api/recipes');
-    const [filter, setFilter] = useState({ ingredients: [] });
+    const [filter, setFilter] = useState({ ingredients: [], search: "" });
 
     const applyFilter = (filter) => {
-        const { ingredients } = filter;
+        const { ingredients, search } = filter;
 
         let result = value;
 
         //Filter by ingredients, result will include recipes that have all ingredients
         if(ingredients) {
             for(let ingredient of ingredients) {
-                result = result.filter(recipe => {
+                result = result?.filter(recipe => {
                     const index = recipe.ingredients.findIndex(ing => ing.id === ingredient.id)
                     return index > -1;
                 });
             }
         }
 
+        //Filter out recipes that do not contain the search text
+        if(search) {
+            const searchText = search.toLowerCase();
+            result = result?.filter(recipe => (
+                recipe.title.toLowerCase().includes(searchText)
+                || recipe.description.toLowerCase().includes(searchText)
+                || recipe.ingredients.map(ing => ing.name).join(" ").toLowerCase().includes(searchText)
+                || recipe.instructions.join(" ").toLowerCase().includes(searchText)
+                || recipe.comments?.join(" ").toLowerCase().includes(searchText)
+            ));
+        }
+
         return result;
     }
 
+    const updateFilter = (filter) => {
+        setFilter(prev => ({ ...prev, ...filter}));
+    }
     
     const createRecipe = async (recipeInfo, callback) => {
         try {
@@ -42,7 +57,7 @@ export default function RecipeContextProvider({ children }) {
     const filteredValue = applyFilter(filter);
     
     return (
-        <recipeContext.Provider value={{ loading, error, data: filteredValue, createRecipe, updateFilter: setFilter, filter }}>
+        <recipeContext.Provider value={{ loading, error, data: filteredValue, createRecipe, updateFilter, filter }}>
             {children}
         </recipeContext.Provider>
     )
