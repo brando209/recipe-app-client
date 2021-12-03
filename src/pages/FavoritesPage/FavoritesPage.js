2561081649bbd788791f657a31c777d2d65e004c
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-12-03 11:44:27 -0800

    Implement `FavoritesPage` to show recipes that are favorited by user

--- src/pages/FavoritesPage/FavoritesPage.js
+++ src/pages/FavoritesPage/FavoritesPage.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Page from '../Page/Page';
+import RecipeCard from '../../components/app/RecipeCard/RecipeCard';
+import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
 
 export default function FavoritesPage(props) {
+    const { data: recipes, loading, error } = useRecipeContext();
+
+    const favoritedRecipes = recipes.filter(recipe => recipe.favorite);
+
     return (
-        <div>Favorites page</div>
-    );
+        <Page>
+            Favorites page
+            {loading && <p>Loading...</p>}
+            {error && <pre>{JSON.stringify(error.message, null, 2)}</pre>}
+            {!error && !loading && favoritedRecipes.map(recipe => (
+                <RecipeCard key={recipe.id} {...recipe} />
+            ))}
+        </Page>
+    )   
 }
