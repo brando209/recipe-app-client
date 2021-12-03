c236be006093878181076bb5fb5842c3f048d903
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-12-03 15:05:25 -0800

    Created `DialogContext` & `useDialogContext` hook

---
+++ src/contexts/DialogContext/DialogContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+export const dialogContext = createContext({});
+
+export const useDialogContext = () => useContext(dialogContext);
+
+export default function DialogContextProvider({ children }) {
+    const [show, setShow] = useState(false);
+    const [dialog, setDialog] = useState({
+        title: "Dialog Box",
+        text: "Dialog body text",
+        footer: <Button>Close</Button>,
+    })
+
+    return (
+        <dialogContext.Provider value={{ dialog, setDialog, show, setShow }}>
+            {children}
+        </dialogContext.Provider>
+    )
+}
