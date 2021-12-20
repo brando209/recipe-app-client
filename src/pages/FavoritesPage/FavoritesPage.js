e55b1e7c0bc67713de4f96852c957b2f3d93b185
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-12-20 12:40:12 -0800

    Fix: navigating to favorites when no recipes in db

--- src/pages/FavoritesPage/FavoritesPage.js
+++ src/pages/FavoritesPage/FavoritesPage.js
@@ -9,7 +9,8 @@ import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
 export default function FavoritesPage(props) {
     const { data: recipes, updateRecipe, loading, error } = useRecipeContext();
 
-    const favoritedRecipes = recipes?.filter(recipe => recipe.favorite);
+    //console.log(recipes) //TODO: Fix: logs <empty string> when no recipes (reason for extra check of length)
+    const favoritedRecipes = recipes && recipes.length > 0 && recipes?.filter(recipe => recipe.favorite);
 
     const handleFavorite = (recipeId, isFavorite) => {
         updateRecipe(recipeId, { favorite: !isFavorite });
