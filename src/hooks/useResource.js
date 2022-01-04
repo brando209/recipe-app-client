0487bea84304704e38a353d763f05c0fd5935b7f
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-01-04 15:16:04 -0800

    `useResource` checks for auth token if `isProtected` prop is true.

--- src/hooks/useResource.js
+++ src/hooks/useResource.js
@@ -5,8 +5,12 @@ const DEFAULT_OPTIONS = {
     headers: { "Content-Type": "application/json" },
 }
 
-export default function useResource(path, options = {}, dependencies = []) {
+export default function useResource(path, options = {}, isProtected = false, dependencies = []) {
     return useAsync(() => {
+        const token = options.headers?.authorization?.split(" ")[1];
+        if(isProtected && (token === null || token === "null" || token === undefined || token === "undefined")) {
+            return Promise.reject("No valid auth token");
+        }
         return axios.get(path, { ...DEFAULT_OPTIONS, ...options }).then(res => {
             if(res.status === 200) return res.data;
         })
