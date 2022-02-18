071c93fbe32855c8a85aada251d991e2099ff826
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-02-18 13:08:42 -0800

    Refactor: `Heart` uses styled component

--- src/components/display/spinners/Heart/Heart.js
+++ src/components/display/spinners/Heart/Heart.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import styled from 'styled-components';
 import { HeartFill } from 'react-bootstrap-icons';
-import './Heart.css';
 
 function Heart() {
     return (
-        <div className="heart-icon-spinner-container">
+        <StyledHeart>
             <HeartFill className='heart-icon-spinner'/>
-        </div>
+        </StyledHeart>
     )
 }
 
 export default Heart;
+
+const StyledHeart = styled.div`
+    width: 50px;
+
+    .heart-icon-spinner {
+        animation-name: heartbeat;
+        animation-duration: 0.5s;
+        animation-iteration-count: infinite;
+        color: red;
+    }
+
+    @keyframes heartbeat {
+        from {
+            height: 1rem;
+            width: 1rem;
+        }
+
+        20% {
+            height: 1rem;
+            width: 1rem;
+        }
+
+        80% {
+            height: 1.2rem; 
+            width: 1.2rem;
+        }
+
+        to {
+            height: 1.2rem; 
+            width: 1.2rem;
+        }
+    }
+`
