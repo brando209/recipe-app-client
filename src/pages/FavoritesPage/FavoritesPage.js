93b929ca4c833fc743f8c1735d31cdb9215c2c63
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-03-11 15:49:07 -0800

    Fix: Favorites page showing all recipes

--- src/pages/FavoritesPage/FavoritesPage.js
+++ src/pages/FavoritesPage/FavoritesPage.js
@@ -5,8 +5,8 @@ import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
 export default function FavoritesPage(props) {
     const { data: recipes, updateRecipe, loading, error } = useRecipeContext();
 
-    const favoritedRecipes = recipes ?? recipes?.filter(recipe => recipe.favorite);
-
+    const favoritedRecipes = recipes && recipes?.filter(recipe => recipe.favorite);
+    
     const handleFavorite = (recipeId, isFavorite) => {
         updateRecipe(recipeId, { favorite: !isFavorite });
     }
@@ -16,7 +16,7 @@ export default function FavoritesPage(props) {
             <h1>Favorites</h1>
             {loading && <p>Loading...</p>}
             {error && <pre>{JSON.stringify(error.message, null, 2)}</pre>}
-            {!error && !loading && <RecipeCards onFavorite={handleFavorite} recipes={favoritedRecipes} />}
+            {!error && !loading && <RecipeCards onFavorite={handleFavorite} recipes={favoritedRecipes || []} />}
         </Page>
     )
 }
