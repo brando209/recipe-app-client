11c283f97fea4aca324a33e60d38294b15dffa97
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-03-15 20:48:15 -0700

    Implement grocery list

--- src/components/form/GroceryListForm/GroceryListForm.js
+++ src/components/form/GroceryListForm/GroceryListForm.js
@@ -1,27 +1,92 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { Formik, Form, Field } from 'formik';
 import InputList from '../../input/InputList/InputList';
 import { XSquare } from 'react-bootstrap-icons';
+import { Button } from 'react-bootstrap';
 
-function GroceryListForm() {
+function GroceryListForm({
+    groceryItems,
+    onItemAdd,
+    onItemCheck,
+    onItemEdit,
+    onItemRemove
+}) {
+    const [editing, setEditing] = useState({ id: null, original: null });
+    const inputRef = useRef(null);
+    const buttonRef = useRef(null);
+
+    const handleFieldBlur = (e) => {
+        if(editing.id) {
+            onItemEdit && e.target.value !== editing.original && onItemEdit(Number(e.target.id), e.target.value)
+            setEditing({ id: null, original: null });
+            return;
+        }
+        if(e.target.value.trim() === "") return;
+        onItemAdd && onItemAdd(e.target.value);
+    }
+
+    const handleSubmit = useCallback(async values => {
+        if(buttonRef.current.disabled) return;
+        buttonRef.current.disabled = true;
+        if(editing.id) {
+            const editedItem = values.items.find(item => item.id === editing.id);
+            onItemEdit && editedItem.name !== editing.original && onItemEdit(editedItem.id, editedItem.name);
+            setEditing({ id: null, original: null });
+            buttonRef.current.disabled = false;
+            return;
+        }
+        const newItem = values.items.find(item => !item.id);
+        if(!newItem) return;
+        onItemAdd && await onItemAdd(newItem.name);
+        buttonRef.current.disabled = false;
+        buttonRef.current.click();
+    }, [editing.id, editing.original, onItemAdd, onItemEdit]);
+
+    //TODO: useCallack instead of ref and useEffect
+    useEffect(() => {
+        if(inputRef.current) inputRef.current.focus();
+    }, [inputRef.current]);
+    
+    const handleNewItem = (addNewItem) => {
+        if(!inputRef.current) {
+            addNewItem();
+        }
+    }
+
+    const handleItemRemove = (itemId) => {
+        onItemRemove && onItemRemove(itemId);
+    }
+
+    const handleEditing = (itemId, originalValue) => {
+        setEditing({ id: itemId, original: originalValue });
+    }
+    
     return (
         <Formik
-            initialValues={{ items: [] }}
-        >{({ values, setFieldValue }) => (
+            initialValues={{ items: groceryItems || [] }}
+            enableReinitialize={true}
+            onSubmit={handleSubmit}
+        >{({ values }) => (
             <Form>
                 <InputList
                     name="items"
                     listItems={values.items}
-                    initialItemValue={{ checked: false, name: "" }}
-                    buttonPlacement="bottom center"
-                    buttonText="Add Grocery Item"
-                    buttonVariant="outline-secondary"
+                    initialItemValue={{ complete: false, name: "" }}
                     renderItem={(item, index, arrayHelpers) => (
-                        <GroceryListItem>
-                            <Field name={`items.${index}.checked`} type="checkbox" />
-                            <Field name={`items.${index}.name`} type="input" placeholder={`Item #${index + 1}`} />
-                            <XSquare onClick={() => arrayHelpers.remove(index)} />
+                        <GroceryListItem key={item.id || `new-${index}`} id={item.id}>
+                            <Field name={`items.${index}.complete`} type="checkbox" onClick={e => onItemCheck(item.id, item.complete)} />
+                            {item.id && editing.id !== item.id ?
+                                <div onClick={() => handleEditing(item.id, item.name)}>{item.name}</div>
+                                :
+                                <Field innerRef={input => { inputRef.current = input; input && input.focus(); }} id={item.id} name={`items.${index}.name`} type="input" placeholder={`Item #${index + 1}`} onBlur={e => handleFieldBlur(e)} />
+                            }
+                            <XSquare onClick={() => { handleItemRemove(item.id); arrayHelpers.remove(index); } } />
                         </GroceryListItem>
+                    )}
+                    buttonPlacement="bottom center"
+                    renderButton={addNewItem => (
+                        <Button ref={buttonRef} variant="outline-secondary" type="button" onClick={() => handleNewItem(addNewItem)}>Add Grocery Item</Button>
                     )}
                 />
             </Form>
@@ -38,6 +103,11 @@ const GroceryListItem = styled.div`
     align-items: center;
     margin: 0.5rem auto;
     gap: 10px;
+
+    >:nth-child(2) {
+        width: 10rem;
+        text-align: left;
+    }
 
     > input {
         width: 100%;
--- src/pages/GroceryListPage/GroceryListPage.js
+++ src/pages/GroceryListPage/GroceryListPage.js
@@ -1,14 +1,78 @@
-import styled from 'styled-components';
 import Page from '../Page/Page';
 import GroceryListForm from '../../components/form/GroceryListForm/GroceryListForm';
+import { useAuth } from '../../contexts/AuthContext/AuthContext';
+import useResource from '../../hooks/useResource';
+import { plannerApi, authHeader } from '../../api';
 
 function GroceryListPage() {
-  return (
-    <Page>
-        <h1>Grocery List</h1>
-        <GroceryListForm />
-    </Page>
-  )
+	const auth = useAuth();
+	const { value: groceryItems, setValue: setGroceryItems, loading } = useResource(
+		'/api/planner/grocery',
+		{ headers:  authHeader(auth.user?.token)},
+		true,
+		[auth.user?.token]
+	);
+
+	const handleItemAdd = async (name) => {
+		if(!name || name === "") return;
+		const newItem = await plannerApi.createGroceryItem({
+			name: name.trim()
+		}, authHeader(auth.user?.token));
+		setGroceryItems(prev => {
+			return [...prev, newItem.data];
+		})
+	}
+
+	const handleItemCheck = (itemId, complete) => {
+		setGroceryItems(prev => {
+			const updatedItems = prev.slice();
+			const updatedIndex = prev.findIndex(item => item.id === itemId);
+			const updatedItem = { ...prev[updatedIndex] };
+			updatedItem.complete = !updatedItem.complete;
+			updatedItems[updatedIndex] = updatedItem;
+			return updatedItems;
+		});
+
+		plannerApi.updateGroceryItem(itemId, { complete: !complete }, authHeader(auth.user?.token));
+	}
+
+	const handleItemEdit = (itemId, newName) => {
+		if(!itemId && (!newName || newName === "")) return;
+		if(!newName || newName === "") return handleItemRemove(itemId);
+
+		setGroceryItems(prev => {
+			const updatedItems = prev.slice();
+			const updatedIndex = prev.findIndex(item => item.id === itemId);
+			const updatedItem = { ...prev[updatedIndex] };
+			updatedItem.name = newName;
+			updatedItems[updatedIndex] = updatedItem;
+			return updatedItems;
+		});
+		plannerApi.updateGroceryItem(itemId, { name: newName }, authHeader(auth.user?.token));
+	}
+
+	const handleItemRemove = (itemId) => {
+		setGroceryItems(prev => {
+			return prev.slice().filter(item => item.id).filter(item => item.id !== itemId);
+		});
+		if(!itemId) return;
+		plannerApi.deleteGroceryItem(itemId, authHeader(auth.user?.token));
+	}
+
+	return (
+		<Page>
+			<h1>Grocery List</h1>
+			{loading ? "Loading..." :
+				<GroceryListForm 
+					groceryItems={groceryItems}
+					onItemAdd={handleItemAdd} 
+					onItemCheck={handleItemCheck} 
+					onItemEdit={handleItemEdit} 
+					onItemRemove={handleItemRemove} 
+				/>
+			}
+		</Page>
+	)
 }
 
 export default GroceryListPage
