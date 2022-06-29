f4d3be848af298a9771bacf6b881c1be7d8743a0
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-06-29 13:59:31 -0700

    Display all decimals as fractions

--- src/components/app/IngredientDisplay/IngredientDisplay.js
+++ src/components/app/IngredientDisplay/IngredientDisplay.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
+const quantityFractionMap = {
+    '0.125':  "1/8",
+    '0.250':  "1/4",
+    '0.330':  "1/3",
+    '0.375':  "3/8",
+    '0.500':  "1/2",
+    '0.625':  "5/8",
+    '0.660':  "2/3",
+    '0.750':  "3/4",
+    '0.875':  "7/8",
+}
+
 function IngredientDisplay({ ingredient }) {
     const { name, quantity, unit, size, comment } = ingredient;
 
-    const formatQuantity = () => {
+    const formatQuantity = (quantity) => {
         if(!quantity) return "";
         if(Number(quantity) % 1 === 0) return `${quantity} `;
 
-        const quantityFractionMap = {
-            '0.125':  "1/8",
-            '0.250':  "1/4",
-            '0.330':  "1/3",
-            '0.375':  "3/8",
-            '0.500':  "1/2",
-            '0.625':  "5/8",
-            '0.660':  "2/3",
-            '0.750':  "3/4",
-            '0.875':  "7/8",
-        }
-
-        return `${Math.floor(quantity) > 0 ? Math.floor(quantity) : ""} ${quantityFractionMap[parseFloat(quantity % 1).toFixed(3)]} `;
+        return `${Math.floor(quantity) > 0 ? Math.floor(quantity) + " " : ""}${quantityFractionMap[parseFloat(quantity % 1).toFixed(3)]} `;
     }
     
     const formatUnit = () => {
@@ -27,14 +27,13 @@ function IngredientDisplay({ ingredient }) {
         if(Number(quantity) > 1) return `${unit}s `
         return `${unit} `;
     }
-    
     const formatName = () => name.trim();
-    const formatSize = () => size ? `, ${size}` : "";
-    const formatComment = () => comment ? ` (${comment})` : "";
+    const formatSize = () => size ? `, ${size} ` : " ";
+    const formatComment = () => comment ? `(${comment.replace(/(\d+)\.(\d+)/, (match) => formatQuantity(match))})` : "";
 
     return (
         <StyledIngredientDisplay>
-            <span id="display-quantity">{formatQuantity()}</span>
+            <span id="display-quantity">{formatQuantity(quantity)}</span>
             <span id="display-unit">{formatUnit()}</span>
             <span id="display-name">{formatName()}</span>
             <span id="display-size">{formatSize()}</span>
